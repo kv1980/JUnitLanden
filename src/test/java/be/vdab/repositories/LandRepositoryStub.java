4e8b0fffdf7e284f3a59ef8c41f8a809c4ad3c80
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-03-14 10:06:47 +0100

    landen met java 1.8

--- src/test/java/be/vdab/repositories/LandRepositoryStub.java
+++ src/test/java/be/vdab/repositories/LandRepositoryStub.java
@@ -3,7 +3,8 @@ package be.vdab.repositories;
 import be.vdab.entities.Land;
 
 public class LandRepositoryStub implements LandRepository {
-
+	
+	@Override
 	public Land read(String landcode) {
 		return new Land(landcode,5);
 	}
