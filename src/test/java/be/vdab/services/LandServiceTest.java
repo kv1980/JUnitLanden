106f5f49652c857e0f5c8469d4b2ce0d36e3cfc4
Kristof Vermeulen <kristofvermeulen1980@gmail.com>
2018-03-14 12:01:51 +0100

    na eerste versie met mock

--- src/test/java/be/vdab/services/LandServiceTest.java
+++ src/test/java/be/vdab/services/LandServiceTest.java
@@ -1,14 +1,18 @@
 package be.vdab.services;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 import java.math.BigDecimal;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import be.vdab.entities.Land;
 import be.vdab.repositories.LandRepository;
-import be.vdab.repositories.LandRepositoryStub;
+//import be.vdab.repositories.LandRepositoryStub;
 
 public class LandServiceTest {
 	private LandRepository landRepository;
@@ -16,13 +20,27 @@ public class LandServiceTest {
 	
 	@Before
 	public void before() {
-		landRepository = new LandRepositoryStub();
+		landRepository = mock(LandRepository.class);
+		when(landRepository.findOppervlakteAlleLanden()).thenReturn(20);
+		when(landRepository.read("B")).thenReturn(new Land("B",5));
+		when(landRepository.read("NL")).thenReturn(new Land("B",6));
+		when(landRepository.read("")).thenThrow(new IllegalArgumentException());
 		landService = new LandService(landRepository);
 	}
 
 	@Test
-	public void findVerhoudingOppervlakteLandTovOppervlakteAlleLanden() {
+	public void findVerhoudingOppervlakteBelgieTovOppervlakteAlleLanden() {
 		assertEquals(0, BigDecimal.valueOf(0.25).compareTo(landService.findVerhoudingOppervlakteLandTovOppervlakteAlleLanden("B")));
+	}
+	
+	@Test
+	public void findVerhoudingOppervlakteNederlandTovOppervlakteAlleLanden() {
+		assertEquals(0, BigDecimal.valueOf(0.30).compareTo(landService.findVerhoudingOppervlakteLandTovOppervlakteAlleLanden("NL")));
+	}
+	
+	@Test (expected = IllegalArgumentException())
+	public void findGeenVerhoudingBijGeenLand {
+		assertEquals(0, BigDecimal.valueOf(0.30).compareTo(landService.findVerhoudingOppervlakteLandTovOppervlakteAlleLanden("NL")));
 	}
 
 }
